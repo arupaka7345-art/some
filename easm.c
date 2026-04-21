ed791bb56ed770b2fdd873ec55fe5a1d8601a0e4
KakinoUni <arupaka7345@gmail.com>
2026-04-21 16:12:30 +0900

    気が向いたら（永遠に）

--- easm.c
+++ easm.c
@@ -1,16 +1,42 @@
+/*
+マップ制作を楽にしたい。
+・もういいy
+*/
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-typedef struct
+typedef enum {false, true} bool;
+
+typedef struct event
 {
 	char *inst;
 	unsigned char mac;
+	struct event *next;
 } event;
 
 event evnew(void);
-evfree(event *);
+bool evfree(event *);
 
 int main()
 {
+	
 	return 0;
+}
+
+event evnew()
+{
+	event e;
+
+	e.inst
+	
+	return e;
+}
+
+bool evfree(event *e)
+{
+	free(e->inst);
+	e->mac = 0;
+
+	return true;
 }
